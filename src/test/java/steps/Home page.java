0a8c9dd66c37e5bba07197b191ebc6686a8c627a
MonaliMane7880 <monalimane7880@gmail.com>
2026-03-05 16:33:01 +0530

    code reverted- Revert "3 line code added home page.java"
    This reverts commit 516e0c771a01614881c3e52eea6682fb19e2ac9f.

--- src/test/java/steps/Home page.java
+++ src/test/java/steps/Home page.java
@@ -8,10 +8,6 @@ public class HomePage {
         System.out.println("2. Search Product");
         System.out.println("3. Logout");
 
-         System.out.println("1. Login");
-        System.out.println("2. Search Product");
-        System.out.println("3. Logout");
-
     }
 
 }
