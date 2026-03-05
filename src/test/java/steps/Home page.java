d8f8eb6ea215cfc7aadea253ee51b70c3eba19fd
MonaliMane7880 <monalimane7880@gmail.com>
2026-03-05 15:42:56 +0530

    Updated code

---
+++ src/test/java/steps/Home page.java
@@ -0,0 +1,13 @@
+public class HomePage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Home Page");
+
+        System.out.println("1. Login");
+        System.out.println("2. Search Product");
+        System.out.println("3. Logout");
+
+    }
+
+}
